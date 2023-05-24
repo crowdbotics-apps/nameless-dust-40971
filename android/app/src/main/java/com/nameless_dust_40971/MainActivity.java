5c07cdc2cb74619c64b055675a6603e41c96f82e
Crowdbotics <team@crowdbotics.com>
2023-05-24 23:37:54 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/nameless_dust_40971/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nameless_dust_40971;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "nameless_dust_40971";
+  }
+}
